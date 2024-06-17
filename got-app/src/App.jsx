1053d0f05343b89418f488301f52c7fc4fdcb315
Gianni Castellano <gianniccastellano@gmail.com>
2024-06-17 11:53:33 -0400

    dbjson backend set up

--- got-app/src/App.jsx
+++ got-app/src/App.jsx
@@ -1,6 +1,17 @@
 import React from "react"
+import { useEffect, useState } from "react"
+
+const baseUrl = "http://localhost:3000"
 
 function App() {
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    fetch(baseUrl + "/characters")
+      .then(resp => resp.json())
+      .then(data => setCharacters(data))
+      
+  })
 
   return (
     <>
