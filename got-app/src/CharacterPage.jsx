ddf78d33d03ff5868f5cb2da1c82ca95581307f5
Gianni Castellano <gianniccastellano@gmail.com>
2024-06-20 07:36:27 -0400

    added button class names

--- got-app/src/CharacterPage.jsx
+++ got-app/src/CharacterPage.jsx
@@ -10,17 +10,17 @@ function CharacterPage( {characters} ) {
 
     return (
         <main>
-            <div>
-                <button onClick={() => handleHouseClick("House Stark")}>House Stark</button>
-                <button onClick={() => handleHouseClick("House Targaryen")}>House Targaryen</button>
-                <button onClick={() => handleHouseClick("House Baratheon")}>House Baratheon</button>
-                <button onClick={() => handleHouseClick("House Lannister")}>House Lannister</button>
-                <button onClick={() => handleHouseClick("House Greyjoy")}>House Greyjoy</button>
-                <button onClick={() => handleHouseClick("House Tyrell")}>House Tyrell</button>
-                <button onClick={() => handleHouseClick("House Bolton")}>House Bolton</button>
-                <button onClick={() => handleHouseClick("House Mormont")}>House Mormont</button>
-                <button onClick={() => handleHouseClick("Free Folk")}>Free Folk</button>
-                <button onClick={() => handleHouseClick("misc")}>Miscellaneous</button>
+            <div className="button-container">
+                <button className="button-bar" onClick={() => handleHouseClick("House Stark")}>House Stark</button>
+                <button className="button-bar" onClick={() => handleHouseClick("House Targaryen")}>House Targaryen</button>
+                <button className="button-bar" onClick={() => handleHouseClick("House Baratheon")}>House Baratheon</button>
+                <button className="button-bar" onClick={() => handleHouseClick("House Lannister")}>House Lannister</button>
+                <button className="button-bar" onClick={() => handleHouseClick("House Greyjoy")}>House Greyjoy</button>
+                <button className="button-bar" onClick={() => handleHouseClick("House Tyrell")}>House Tyrell</button>
+                <button className="button-bar" onClick={() => handleHouseClick("House Bolton")}>House Bolton</button>
+                <button className="button-bar" onClick={() => handleHouseClick("House Mormont")}>House Mormont</button>
+                <button className="button-bar" onClick={() => handleHouseClick("Free Folk")}>Free Folk</button>
+                <button className="button-bar" onClick={() => handleHouseClick("misc")}>Miscellaneous</button>
             </div>
             <CharacterList characters={characters} selectedHouse={selectedHouse} />
         </main>
